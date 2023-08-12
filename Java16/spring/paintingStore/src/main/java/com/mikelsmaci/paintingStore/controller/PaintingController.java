fee2ec065289810969aae4b439112082d089f8d5
mikelsma <smaci099@gmail.com>
2023-08-12 17:56:40 +0200

    Adding painting java class, painting controller wiht spring annotations and html files paintingList and paintingRegister.

--- Java16/spring/paintingStore/src/main/java/com/mikelsmaci/paintingStore/controller/PaintingController.java
+++ Java16/spring/paintingStore/src/main/java/com/mikelsmaci/paintingStore/controller/PaintingController.java
@@ -12,4 +12,16 @@ public class PaintingController {
     public String home() {
         return "home";
     }
+    //we add a path for paintingRegister
+    @GetMapping("/painting_register")
+    public String paintingRegister() {
+        return "paintingRegister";
+        //now we go to template and create a new html file paitingRegister
+    }
+    // we add a path for available_paintings
+    @GetMapping("/available_paintings")
+    public String getAllPaintings() {
+        return "paintingList";
+        //now we go to template and create a new html file paintingList
+    }
 }
---
+++ Java16/spring/paintingStore/src/main/java/com/mikelsmaci/paintingStore/entity/Painting.java
@@ -0,0 +1,65 @@
+package com.mikelsmaci.paintingStore.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Painting {
+    //we add id attribute
+    @Id
+    //we add annotation for generating incremented id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String author;
+    private String price;
+    //then we add a constructor
+
+    public Painting(int id, String name, String author, String price) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+    //then we create a constructor for supper class
+    public Painting() {
+        super();
+        //TODO Auto-generated constructor stub
+    }
+    //then we generate getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    //then we go to application properties and add properties for connection to database
+}
