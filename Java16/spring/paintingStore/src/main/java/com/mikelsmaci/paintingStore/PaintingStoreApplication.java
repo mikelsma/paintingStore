6c483ccd5dfdbb7353cb5e2d91c37f07aaa4a70e
mikelsma <smaci099@gmail.com>
2023-08-12 08:29:11 +0200

    Added mysql, database driver and jpa properties at application.properties

---
+++ Java16/spring/paintingStore/src/main/java/com/mikelsmaci/paintingStore/PaintingStoreApplication.java
@@ -0,0 +1,13 @@
+package com.mikelsmaci.paintingStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintingStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintingStoreApplication.class, args);
+	}
+
+}
---
+++ Java16/spring/paintingStore/src/test/java/com/mikelsmaci/paintingStore/PaintingStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikelsmaci.paintingStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintingStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
